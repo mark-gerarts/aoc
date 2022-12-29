9dfed679fe4442b193b1b09e88cc18e504f22ae5
Mark Gerarts <mark.gerarts@gmail.com>
2022-12-29 20:28:49 +0100

    Solve day 6

---
+++ 2022/aoc-2022-cl/src/06.lisp
@@ -0,0 +1,14 @@
+(defpackage day-06
+  (:use :cl :alexandria :serapeum))
+(in-package :day-06)
+
+(defun first-unique-window (input window-size)
+  (loop for i from 0 to (- (length input) window-size)
+        for window = (subseq input i (+ window-size i))
+          when (string= (nub window) window)
+          return (+ i window-size)))
+
+(defun solve ()
+  (let ((input (str:from-file "./input/06.txt")))
+    (format t "Part 1: ~A~%" (first-unique-window input 4))
+    (format t "Part 2: ~A" (first-unique-window input 14))))
