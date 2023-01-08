a0f19033cf8b52ff412930ef9d9e245c1ca44190
Mark Gerarts <mark.gerarts@gmail.com>
2023-01-08 22:07:15 +0100

    Solve 09.b

--- 2022/aoc-2022-cl/src/09.lisp
+++ 2022/aoc-2022-cl/src/09.lisp
@@ -11,16 +11,6 @@
        (fixnump (first thing))
        (fixnump (second thing))))
 
-(deftype state () '(satisfies state-p))
-
-(-> state-p (t) boolean)
-(defun state-p (thing)
-  (and (hash-table-p thing)
-       (@ thing :pos-head)
-       (@ thing :pos-tail)
-       (@ thing :seen)
-       t))
-
 (-> add (pos pos) pos)
 (defun add (pos-a pos-b)
   (list (+ (first pos-a) (first pos-b))
@@ -31,36 +21,44 @@
   (list (- (first pos-a) (first pos-b))
         (- (second pos-a) (second pos-b))))
 
-(-> move-tail (pos pos) pos)
-(defun move-tail (pos-head pos-tail)
-  (add pos-tail (trivia:match (diff pos-head pos-tail)
-                  ;; Up/down/left/right
-                  ('(0 2) '(0 1))
-                  ('(0 -2) '(0 -1))
-                  ('(2 0) '(1 0))
-                  ('(-2 0) '(-1 0))
-                  ;; Diagonal (chess horse)
-                  ('(1 2) '(1 1))
-                  ('(-1 2) '(-1 1))
-                  ('(1 -2) '(1 -1))
-                  ('(-1 -2) '(-1 -1))
-                  ('(2 1) '(1 1))
-                  ('(2 -1) '(1 -1))
-                  ('(-2 1) '(-1 1))
-                  ('(-2 -1) '(-1 -1))
-                  ;; T/H are on top of eachother or already correctly
-                  ;; positioned.
-                  (_ '(0 0)))))
+(defun get-segment-pos (segment prev)
+  "Calculates the new position of a tail segment given the previous segment."
+  (add segment
+       (trivia:match (diff prev segment)
+         ;; Up/down/left/right
+         ('(0 2) '(0 1))
+         ('(0 -2) '(0 -1))
+         ('(2 0) '(1 0))
+         ('(-2 0) '(-1 0))
+         ;; Diagonal (chess horse)
+         ('(1 2) '(1 1))
+         ('(-1 2) '(-1 1))
+         ('(1 -2) '(1 -1))
+         ('(-1 -2) '(-1 -1))
+         ('(2 1) '(1 1))
+         ('(2 -1) '(1 -1))
+         ('(-2 1) '(-1 1))
+         ('(-2 -1) '(-1 -1))
+         ;; Diagonal (regular)
+         ('(2 2) '(1 1))
+         ('(2 -2) '(1 -1))
+         ('(-2 2) '(-1 1))
+         ('(-2 -2) '(-1 -1))
+         ;; T/H are on top of eachother or already correctly
+         ;; positioned.
+         (_ '(0 0)))))
 
-;; Use state hash table instead, returning new dict?
-(-> apply-step (state pos) state)
-(defun apply-step (state step)
-  (let* ((new-pos-head (add (@ state :pos-head) step))
-         (new-pos-tail (move-tail new-pos-head (@ state :pos-tail)))
-         (new-seen (adjoin new-pos-tail (@ state :seen) :test #'equal)))
-    (dict :pos-head new-pos-head
-          :pos-tail new-pos-tail
-          :seen new-seen)))
+(-> move-rope (list pos) list)
+(defun move-rope (rope step)
+  (loop for segment in rope
+        for i from 0
+          if (zerop i)
+          ;; Update the head position.
+        collect (add segment step) into new-rope
+          else
+          ;; Update tail segments based on the previous one.
+        collect (get-segment-pos segment (nth (1- i) new-rope)) into new-rope
+        finally (return new-rope)))
 
 (defun parse-input ()
   (loop for line in (uiop:read-file-lines "./input/09.txt")
@@ -70,15 +68,16 @@
                   ((trivia.ppcre:ppcre "L (\\d+)" steps) `((-1 0) ,steps))
                   ((trivia.ppcre:ppcre "R (\\d+)" steps) `((1 0) ,steps)))))
 
-(defun solve-a ()
-  (loop with state = (dict :pos-head '(0 0)
-                           :pos-tail '(0 0)
-                           :seen '((0 0)))
+(-> solve (fixnum) fixnum)
+(defun solve (num-segments)
+  (loop with rope = (make-list num-segments :initial-element '(0 0))
+        with seen = '((0 0))
         for (pos steps) in (parse-input)
         do (loop repeat (parse-integer steps)
-                 for new-state = (apply-step state pos)
-                 do (setf state new-state))
-        finally (return (length (@ state :seen)))))
+                 do (setf rope (move-rope rope pos))
+                 do (setf seen (adjoin (car (last rope)) seen :test #'equal)))
+        finally (return (length seen))))
 
-(defun solve ()
-  (format t "Part A: ~A~%" (solve-a)))
+(defun solve-both ()
+  (format t "Part A: ~A~%" (solve 2))
+  (format t "Part B: ~A~%" (solve 10)))
