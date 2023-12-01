67b9b5881c014daec1d8115ab2eec0f2c9fa55db
Mark Gerarts <mark.gerarts@gmail.com>
2023-12-01 11:39:20 +0100

    Simplify logic day 1

--- 2023/aoc-2023-fs/Day01.fs
+++ 2023/aoc-2023-fs/Day01.fs
@@ -1,61 +1,29 @@
 module AoC2023.Day01
 
-type Direction =
-    | Left
-    | Right
-
-let replaceDigitWords direction (line: string) =
-    let tryReplace (line: string) =
-        line
-            .Replace("one", "1")
-            .Replace("two", "2")
-            .Replace("three", "3")
-            .Replace("four", "4")
-            .Replace("five", "5")
-            .Replace("six", "6")
-            .Replace("seven", "7")
-            .Replace("eight", "8")
-            .Replace("nine", "9")
-
-    let charsToString cs = cs |> Array.ofList |> System.String
-
-    // Take larger and larger prefixes to try and replace on.
-    let rec goFromLeft prefix suffix =
-        match suffix with
-        | [] -> charsToString prefix
-        | c :: cs ->
-            let prefix = charsToString (prefix @ [ c ])
-            goFromLeft (tryReplace prefix |> Seq.toList) cs
-
-    // Similar, but taking larger and larger suffixes.
-    let rec goFromRight prefix suffix =
-        match prefix with
-        | [] -> charsToString suffix
-        | _ ->
-            let last = List.last prefix
-            let init = List.take (List.length prefix - 1) prefix
-            let suffix = charsToString (last :: suffix)
-            goFromRight init (tryReplace suffix |> Seq.toList)
-
-    match direction with
-    | Left -> goFromLeft [] (line |> Seq.toList)
-    | Right -> goFromRight (line |> Seq.toList) []
+let replaceDigitWords (line: string) =
+    // Replacements are like "on1ne" to still keep overlaps.
+    line
+        .Replace("one", "on1ne")
+        .Replace("two", "tw2wo")
+        .Replace("three", "th3ree")
+        .Replace("four", "fo4ur")
+        .Replace("five", "fi5ve")
+        .Replace("six", "si6x")
+        .Replace("seven", "se7ven")
+        .Replace("eight", "ei8ght")
+        .Replace("nine", "ni9ne")
 
 let firstDigit line =
-    line |> Seq.find System.Char.IsDigit |> (fun c -> int c - int '0')
+    line |> Seq.find System.Char.IsDigit |> (string >> int)
 
-let solve lefts rights =
-    let leftSum = lefts |> Seq.sumBy firstDigit
-    let rightSum = rights |> Seq.sumBy (Seq.rev >> firstDigit)
+let solve lines =
+    let leftSum = lines |> Seq.sumBy firstDigit
+    let rightSum = lines |> Seq.sumBy (Seq.rev >> firstDigit)
 
     leftSum * 10 + rightSum
 
 let run filename =
     let lines = filename |> System.IO.File.ReadAllLines
 
-    solve lines lines |> printfn "Part 1: %i"
-
-    let lefts = lines |> Seq.map (replaceDigitWords Left)
-    let rights = lines |> Seq.map (replaceDigitWords Right)
-
-    solve lefts rights |> printfn "Part 2: %i"
+    lines |> solve |> printfn "Part 1: %i"
+    lines |> Seq.map replaceDigitWords |> solve |> printfn "Part 2: %i"
