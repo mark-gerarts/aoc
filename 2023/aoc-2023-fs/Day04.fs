a212616a5364e85f91e88e6d1e8abb573ddb7bf4
Mark Gerarts <mark.gerarts@gmail.com>
2023-12-04 09:50:30 +0100

    Solve day 4

--- 2023/aoc-2023-fs/Day04.fs
+++ 2023/aoc-2023-fs/Day04.fs
@@ -1,3 +1,38 @@
 module AoC2023.Day04
 
-let run filename = ()
+open System.Text.RegularExpressions
+
+let parseLine (line: string) =
+    let extractDigits line =
+        [ for m in Regex.Matches(line, "\d+") -> int m.Value ]
+
+    match (line.Split(':')[1]).Split('|') with
+    | [| left; right |] -> extractDigits left, extractDigits right
+    | _ -> failwithf "Incorrect input line %s" line
+
+let matchingNumbers (winningNumbers, actualNumbers) =
+    Set.intersect (Set.ofList winningNumbers) (Set.ofList actualNumbers)
+    |> Set.count
+
+let part2 scores =
+    let rec go scores bonusCards totalCards =
+        match scores, bonusCards with
+        | [], _ -> totalCards
+        | _, [] -> failwith "Out of bonuses"
+        | score :: rest, bonus :: bonusCards ->
+            let newHead = bonusCards |> List.take score |> List.map ((+) bonus)
+            let newTail = bonusCards |> List.skip score
+
+            go rest (newHead @ newTail) (totalCards + bonus)
+
+    let bonusCards = List.replicate (List.length scores) 1
+    go scores bonusCards 0
+
+let run filename =
+    let scores =
+        filename
+        |> System.IO.File.ReadAllLines
+        |> Seq.map (parseLine >> matchingNumbers)
+
+    scores |> Seq.sumBy (fun s -> pown 2 (s - 1)) |> printfn "Part 1: %i"
+    scores |> Seq.toList |> part2 |> printfn "Part 2: %i"
