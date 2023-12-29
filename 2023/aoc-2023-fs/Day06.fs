415ba797052187ff93dc5e6b6cf1a4f81f4ac57f
Mark Gerarts <mark.gerarts@gmail.com>
2023-12-29 23:05:39 +0100

    Solve 2023.06

--- 2023/aoc-2023-fs/Day06.fs
+++ 2023/aoc-2023-fs/Day06.fs
@@ -1,3 +1,29 @@
 module AoC2023.Day06
 
-let run filename = ()
+open System.Text.RegularExpressions
+
+let lineParserPart1 line =
+    [ for x in Regex("\d+").Matches(line) -> int64 x.Value ]
+
+let lineParserPart2 line =
+    [ [ for x in Regex("\d+").Matches(line) -> x.Value ] |> String.concat "" |> int64 ]
+
+let parse lineParser filename =
+    let inputs =
+        filename |> System.IO.File.ReadAllLines |> Seq.toList |> List.map lineParser
+
+    match inputs with
+    | [ times; distances ] -> List.zip times distances
+    | _ -> failwith "Invalid input"
+
+let solve races =
+    let winningTimes (ms, record) =
+        [ 1L .. ms - 1L ]
+        |> List.map (fun holdDownMs -> (ms - holdDownMs) * holdDownMs)
+        |> List.filter ((<) record)
+
+    races |> List.map winningTimes |> List.map List.length |> List.reduce ((*))
+
+let run filename =
+    filename |> parse lineParserPart1 |> solve |> printfn "Part 1: %A"
+    filename |> parse lineParserPart2 |> solve |> printfn "Part 2: %A"
