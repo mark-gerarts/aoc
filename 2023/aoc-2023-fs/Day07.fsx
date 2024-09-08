493d1b268415fdfa90b319a194ae6ec9d110526c
Mark Gerarts <mark.gerarts@gmail.com>
2024-09-08 13:22:20 +0200

    Solve 2023.7

--- 2023/aoc-2023-fs/Day07.fsx
+++ 2023/aoc-2023-fs/Day07.fsx
@@ -12,27 +12,22 @@ type HandType =
     | FiveOfAKind
 
 let handType hand =
-    let cards = Seq.toList hand
-    let cardCounts = cards |> Seq.countBy id |> Seq.map snd |> Seq.toList
+    let cardCounts =
+        hand
+        |> Seq.toList
+        |> Seq.countBy id
+        |> Seq.map snd
+        |> Seq.sortByDescending id
+        |> Seq.toList
 
-    let numPairs cardCounts =
-        cardCounts
-        |> List.countBy id
-        |> Map.ofList
-        |> Map.tryFind 2
-        |> Option.defaultValue 0
-
-    let matchers =
-        [ cardCounts = [ 5 ], FiveOfAKind
-          List.contains 4 cardCounts, FourOfAKind
-          cardCounts = [ 3; 2 ], FullHouse
-          cardCounts = [ 2; 3 ], FullHouse
-          List.contains 3 cardCounts, ThreeOfAKind
-          numPairs cardCounts = 2, TwoPair
-          numPairs cardCounts = 1, OnePair
-          true, HighCard ]
-
-    matchers |> List.find ((fst)) |> snd
+    match cardCounts with
+    | [ 5 ] -> FiveOfAKind
+    | [ 4; 1 ] -> FourOfAKind
+    | [ 3; 2 ] -> FullHouse
+    | [ 3; 1; 1 ] -> ThreeOfAKind
+    | [ 2; 2; 1 ] -> TwoPair
+    | [ 2; 1; 1; 1 ] -> OnePair
+    | _ -> HighCard
 
 let rank card =
     match card with
@@ -43,16 +38,16 @@ let rank card =
     | 'T' -> 10
     | c -> int c - int '0'
 
-let rec compareCards (hand1: string) (hand2: string) =
-    let hand1 = Seq.map rank hand1
-    let hand2 = Seq.map rank hand2
+let rec compareCards rankFn (hand1: string) (hand2: string) =
+    let hand1 = Seq.map rankFn hand1
+    let hand2 = Seq.map rankFn hand2
 
     Seq.compareWith compare hand1 hand2
 
-let compareHands (hand1: string) (hand2: string) =
+let compareHands handTypeFn rankFn (hand1: string) (hand2: string) =
     let comparison =
-        match compare (handType hand1) (handType hand2) with
-        | 0 -> compareCards hand1 hand2
+        match compare (handTypeFn hand1) (handTypeFn hand2) with
+        | 0 -> compareCards rankFn hand1 hand2
         | comparison -> comparison
 
     match comparison with
@@ -65,13 +60,46 @@ let parseLine (line: string) =
     | [| hand; bid |] -> hand, int bid
     | _ -> failwithf "Failed to parse line %s" line
 
-let input = System.IO.File.ReadLines "input/07.txt" |> Seq.map parseLine
+let solve handTypeFn rankFn =
+    "input/07.test"
+    |> System.IO.File.ReadLines
+    |> Seq.map parseLine
+    |> Seq.sortWith (fun a b -> compareHands handTypeFn rankFn (fst a) (fst b))
+    |> Seq.mapi (fun i (_, bid) -> (i + 1) * bid)
+    |> Seq.sum
 
-input
-|> Seq.sortWith (fun a b -> compareHands (fst a) (fst b))
-|> Seq.mapi (fun i (_, bid) -> (i + 1) * bid)
-|> Seq.sum
-|> printfn "Part 1: %i"
+//
+// Part 2
+//
+
+let jokerize hand =
+    let replacements = [ '2' .. '9' ] @ [ 'T'; 'Q'; 'K'; 'A' ]
+
+    let rec variations hand =
+        seq {
+            match hand with
+            | [] -> yield []
+            | 'J' :: rest ->
+                for r in replacements do
+                    for rest' in variations rest do
+                        yield r :: rest'
+            | c :: rest ->
+                for rest' in variations rest do
+                    yield c :: rest'
+        }
+
+    variations (Seq.toList hand)
+
+let handTypeP2 hand =
+    hand |> jokerize |> Seq.map handType |> Seq.max
+
+let rankP2 card =
+    match card with
+    | 'J' -> 1
+    | c -> rank c
+
+solve handType rank |> printfn "Part 1: %i"
+solve handTypeP2 rankP2 |> printfn "Part 2: %i"
 
 handType "QQQQQ" |> should equal FiveOfAKind
 handType "QQQQ1" |> should equal FourOfAKind
@@ -82,9 +110,11 @@ handType "77122" |> should equal TwoPair
 handType "912K9" |> should equal OnePair
 handType "12345" |> should equal HighCard
 
-compareCards "A" "K" |> should equal 1
-compareCards "3" "9" |> should equal -1
-compareCards "T" "T" |> should equal 0
+compareCards rank "A" "K" |> should equal 1
+compareCards rank "3" "9" |> should equal -1
+compareCards rank "T" "T" |> should equal 0
 
-compareHands "KK677" "KTJJT" |> should equal 1
-compareHands "T55J5" "QQQJA" |> should equal -1
+compareHands handType rank "KK677" "KTJJT" |> should equal 1
+compareHands handType rank "T55J5" "QQQJA" |> should equal -1
+
+compareCards rankP2 "J" "2" |> should equal -1
