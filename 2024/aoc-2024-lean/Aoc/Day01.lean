2198fbbae672d4ab0cbba85d17dd26bf602bdb41
Mark Gerarts <mark.gerarts@gmail.com>
2025-02-11 21:22:13 +0100

    Implement day 01 in lean

--- 2024/aoc-2024-lean/Aoc/Day01.lean
+++ 2024/aoc-2024-lean/Aoc/Day01.lean
@@ -1,4 +1,33 @@
-def main : IO Unit :=
-  IO.println "Solving Day 01!"
+def parseInput (input: String): List Int × List Int :=
+    let pairs :=
+      input.trim.splitOn "\n"
+      |> List.map (·.splitOn "   ")
 
-#eval main
+    let lefts := pairs |> List.map (·[0]!.toInt!)
+    let rights := pairs |> List.map (·[1]!.toInt!)
+
+    (lefts, rights)
+
+
+def part1 (lefts rights: List Int): Nat :=
+    List.zip (List.mergeSort lefts) (List.mergeSort rights)
+    |> List.map (λ (l, r) => l - r)
+    |> List.map Int.natAbs
+    |> List.sum
+
+
+def part2 (lefts rights: List Int): Int :=
+  let similarityScore l :=
+    rights |> List.filter (· = l) |> List.length
+
+  lefts
+  |> List.map (λ l => l * similarityScore l)
+  |> List.sum
+
+
+def main : IO Unit := do
+  let input ← IO.FS.readFile "input/01.txt"
+  let (ls, rs) := parseInput input
+
+  s!"Part 1: {part1 ls rs}" |> IO.println
+  s!"Part 2: {part2 ls rs}" |> IO.println
