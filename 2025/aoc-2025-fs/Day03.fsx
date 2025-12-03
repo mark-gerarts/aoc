c3eb9362abb10eade948563c82387280f8536638
Mark Gerarts <mark.gerarts@gmail.com>
2025-12-03 19:45:24 +0100

    Solve 2025.03

--- 2025/aoc-2025-fs/Day03.fsx
+++ 2025/aoc-2025-fs/Day03.fsx
@@ -1,32 +1,38 @@
-let parseInput =
-    "input/03.txt"
-    |> System.IO.File.ReadLines
-    |> Seq.map (Seq.map (string >> int) >> Seq.toList)
+#r "nuget: FsUnit,7.1.1"
 
-// Finds all permutations where the order of the original input list is
-// preserved. E.g. [1; 2; 3] => [1; 2] [1; 3] [2; 3]
-let rec consecutivePermutationsOfLength length xs =
-    seq {
-        if length = 1 then
-            yield! List.map List.singleton xs
-        elif length > List.length xs then
-            ()
-        else
-            let head, tail = List.head xs, List.tail xs
+open FsUnit
 
-            yield! consecutivePermutationsOfLength length tail
+let parseLine = Seq.map (string >> int64) >> Seq.toList
 
-            for t in consecutivePermutationsOfLength (length - 1) tail do
-                yield head :: t
-    }
+let parseInput = "input/03.txt" |> System.IO.File.ReadLines |> Seq.map parseLine
 
+let listToInt = List.rev >> List.mapi (fun i x -> x * pown 10L i) >> List.sum
 
-let listToInt xs =
-    xs |> List.rev |> List.mapi (fun i x -> x * pown 10 i) |> List.sum
+let rec largestSubsequence length xs =
+    let rec candidateDigits xs =
+        match xs with
+        | x :: xs' when List.length xs >= length -> (x, xs') :: candidateDigits xs'
+        | _ -> []
 
+    if length = 0 then
+        []
+    else
+        let largestDigit, tail = candidateDigits xs |> List.maxBy fst
 
-let maxJoltage battery =
-    consecutivePermutationsOfLength 2 battery |> Seq.map listToInt |> Seq.max
+        largestDigit :: largestSubsequence (length - 1) tail
 
-// TODO: this is okayish for n=2, but explodes for n=12 (ofcourse).
-parseInput |> Seq.sumBy maxJoltage |> printfn "%A"
+let maxJoltage numBatteries bank =
+    largestSubsequence numBatteries bank |> listToInt
+
+parseInput |> Seq.sumBy (maxJoltage 2) |> printfn "Part 1: %i"
+parseInput |> Seq.sumBy (maxJoltage 12) |> printfn "Part 2: %i"
+
+"987654321111111" |> parseLine |> maxJoltage 2 |> should equal 98
+"811111111111119" |> parseLine |> maxJoltage 2 |> should equal 89
+"234234234234278" |> parseLine |> maxJoltage 2 |> should equal 78
+"818181911112111" |> parseLine |> maxJoltage 2 |> should equal 92
+
+"987654321111111" |> parseLine |> maxJoltage 12 |> should equal 987654321111L
+"811111111111119" |> parseLine |> maxJoltage 12 |> should equal 811111111119L
+"234234234234278" |> parseLine |> maxJoltage 12 |> should equal 434234234278L
+"818181911112111" |> parseLine |> maxJoltage 12 |> should equal 888911112111L
