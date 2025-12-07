1466c3e47f6a0672d565e7b7a90f381d1830dea0
Mark Gerarts <mark.gerarts@gmail.com>
2025-12-07 22:30:44 +0100

    Solve 2025.07

---
+++ 2025/aoc-2025-fs/Day07.fsx
@@ -0,0 +1,48 @@
+let down (x, y) = x, y + 1
+let left (x, y) = x - 1, y
+let right (x, y) = x + 1, y
+
+let input = System.IO.File.ReadLines "input/07.txt"
+
+let startPos = input |> Seq.head |> _.IndexOf("S"), 0
+let firstRow = (startPos, 1L) |> Seq.singleton |> Map.ofSeq
+
+// Mutable counter was just easier...
+let mutable numSplits = 0
+
+let splitterPositions =
+    seq {
+        for y, line in System.IO.File.ReadLines "input/07.txt" |> Seq.indexed do
+            for x, cell in Seq.indexed line do
+                if cell = '^' then
+                    x, y
+    }
+    |> Set.ofSeq
+
+let _, maxY = splitterPositions |> Set.maxElement
+
+let nextRow prevRow =
+    let newRow =
+        seq {
+            for pos, count in prevRow |> Map.toSeq do
+                if Set.contains (down pos) splitterPositions then
+                    numSplits <- numSplits + 1
+                    yield left (down pos), count
+                    yield right (down pos), count
+                else
+                    yield down pos, count
+        }
+
+    newRow
+    |> Seq.groupBy fst
+    |> Seq.map (fun (p, cs) -> p, Seq.sumBy snd cs)
+    |> Map.ofSeq
+
+let lastRow =
+    Seq.initInfinite id
+    |> Seq.scan (fun row _ -> nextRow row) firstRow
+    |> Seq.skip maxY
+    |> Seq.head
+
+numSplits |> printfn "Part 1: %i"
+lastRow |> Map.values |> Seq.sum |> printfn "Part 2: %i"
