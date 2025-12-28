d94625afcd616dab736325de3399be9448ee267f
Mark Gerarts <mark.gerarts@gmail.com>
2025-12-28 21:09:11 +0100

    Solve 2016.03

--- 2016/aoc-2016-fs/Day03.fsx
+++ 2016/aoc-2016-fs/Day03.fsx
@@ -1,3 +1,24 @@
-module AoC2016.Day03
+open System.Text.RegularExpressions
 
-let run filename = ()
+let parseLine (line: string) =
+    Regex("\s+").Split(line.Trim()) |> Seq.map int |> Seq.toList
+
+let isValidTriangle sides =
+    match List.sort sides with
+    | [a; b; c] -> a + b > c
+    | _ -> false
+
+let countValidTriangles triangles =
+    triangles |> Seq.filter isValidTriangle |> Seq.length
+
+let input = System.IO.File.ReadAllLines "input/03.txt" |> Seq.map parseLine
+
+input |> countValidTriangles |> printfn "Part 1: %i"
+
+input
+|> Seq.transpose
+|> Seq.collect id
+|> Seq.splitInto (Seq.length input)
+|> Seq.map Seq.toList
+|> countValidTriangles
+|> printfn "Part 2: %i"
