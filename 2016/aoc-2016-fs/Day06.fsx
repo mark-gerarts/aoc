677f843ae4974c0609adc10eb6896f48a7151131
Mark Gerarts <mark.gerarts@gmail.com>
2026-01-05 21:58:32 +0100

    Solve 2016.06, 2016.07a

--- 2016/aoc-2016-fs/Day06.fsx
+++ 2016/aoc-2016-fs/Day06.fsx
@@ -1,3 +1,12 @@
-module AoC2016.Day06
+let leastAndMostFrequentElement xs =
+    let sorted = xs |> Seq.countBy id |> Seq.sortBy snd |> Seq.map fst
 
-let run filename = ()
+    Seq.head sorted, Seq.last sorted
+
+let chars =
+    System.IO.File.ReadAllLines "input/06.txt"
+    |> Seq.transpose
+    |> Seq.map leastAndMostFrequentElement
+
+chars |> Seq.map snd |> System.String.Concat |> printfn "Part 1: %s"
+chars |> Seq.map fst |> System.String.Concat |> printfn "Part 2: %s"
--- 2016/aoc-2016-fs/Day07.fsx
+++ 2016/aoc-2016-fs/Day07.fsx
@@ -1,3 +1,42 @@
-module AoC2016.Day07
+#r "nuget: FParsec,1.1.1"
 
-let run filename = ()
+open FParsec
+
+type Sequence =
+    | Normal of string
+    | Hypernet of string
+
+let value sequence =
+    match sequence with
+    | Normal s -> s
+    | Hypernet s -> s
+
+let parse line =
+    let pAlpha = regex "[a-z]+"
+    let pHypernet = pchar '[' >>. pAlpha .>> pchar ']' |>> Hypernet
+    let pNormal = pAlpha |>> Normal
+
+    let pIPv7 = many (pHypernet <|> pNormal)
+
+    match run pIPv7 line with
+    | Success(out, _, _) -> out
+    | _ -> failwithf "Unable to parse IPv7 %s" line
+
+let containsABBA s =
+    s |> Seq.windowed 4 |> Seq.exists (fun s -> s = Array.rev s && s[0] <> s[1])
+
+let supportsTLS ipv7 =
+    let parts = parse ipv7
+
+    let abbaInNormal =
+        parts |> Seq.filter _.IsNormal |> Seq.exists (value >> containsABBA)
+
+    let abbaInHyper =
+        parts |> Seq.filter _.IsHypernet |> Seq.exists (value >> containsABBA)
+
+    abbaInNormal && not abbaInHyper
+
+System.IO.File.ReadAllLines "input/07.txt"
+|> Seq.filter supportsTLS
+|> Seq.length
+|> printfn "Part 1: %i"
